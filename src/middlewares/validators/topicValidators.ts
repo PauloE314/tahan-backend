fcc5b401f96b83b1af93afa01e904f08a60cca6f
unknown <pauloeduardodelima155@gmail.com>
2020-06-27 12:39:03 -0300

    separação de topics e sections

---
+++ src/middlewares/validators/topicValidators.ts
@@ -0,0 +1,10 @@
+import { APIRequest } from "src/@types/global";
+import { Response, NextFunction } from "express";
+
+import { Topics } from '@models/Topics';
+import { getRepository } from "typeorm";
+
+
+export default class TopicValidator {
+ 
+}
