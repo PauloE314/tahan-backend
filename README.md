b59dcfee2ab852e87e56b403af566d1d2cc73414
unknown <pauloeduardodelima155@gmail.com>
2020-11-05 08:12:39 -0300

    Update no readme

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # **Tahan**
 
-Tahan é um projeto que promete tornar o aprendizado divertido e produtivo ao mesmo tempo. Segue o funcionamento da API dessa aplicação dividido em seções:
+<p align="center">
+  <img src="https://i.ibb.co/kcC2FsQ/Lorem.png" style="border-radius: 15px; max-width: 250px; width: 100%">
+</p><br>
+
+**Tahan** é um projeto que promete tornar o aprendizado divertido e produtivo ao mesmo tempo. Trata-se de uma plataforma online em que professores podem criar postagens, enviar conteúdos e criar quizzes multiplayer ou singleplayer, tudo isso de forma simples e intuitiva.
+
+O atual repositório é o sistema backend, feito sob os parâmetros API Restful, do projeto **Tahan**, no geral ele terá sistema [mobile](https://github.com/petruspierre/tahan) e [fronted](https://github.com/RaquelPM/TahanFront) também.
+
+## **Recursos**
+
+- Cadastro e login de contas de usuário com Google (diferenciando-os entre alunos e professores)
+- Sistema de criação, manutenção e leitura de postagens sobre um dado assunto (material de estudo)
+- Sistema de agrupamento de postagens em containers no perfil do professor
+- Criação de quizzes por parte dos professores
+- Sistema de quizzes multiplayer e singleplayer em tempo real.
+- Sistema simples de amizade para auxiliar a criação de "salas de jogo"
+
+## **Principais tecnologias**
+
+- Node.js v12.17
+- Express.js v4.17.1
+- Socket.io v2.3.0
+- TypeScript v3.3.333
+- Sqlite v4.0.3
+
+## **Funcionamento**
+
+Segue o funcionamento da API dessa aplicação dividido em seções:
 
 - [Configurações Gerais](./docs/general.md)
 - [Usuários](./docs/users.md)
--- src/utils/baseError.ts
+++ src/utils/baseError.ts
@@ -1,47 +1,47 @@
 import { SocketEvents } from "@config/socket";
 import { APISocket } from "src/@types/socket";
 import { Server } from "socket.io";
-import { SocketClient } from "src/socket/general/clients";
+import { SocketClient } from "src/socket/entities/clients";
 /**
  * Erro base da aplicação que permite a utilização de vários dados, não apenas strings
  */
 export class Err extends Error {
-    data: any;
-    
-    constructor(name: string, data: any) {
-        super();
-        this.name = name;
-        this.data = {
-            name,
-            data
-        }
-    }
+  data: any;
+
+  constructor(name: string, data: any) {
+    super();
+    this.name = name;
+    this.data = {
+      name,
+      data,
+    };
+  }
 }
 
 export interface IGameException {
-    name: string,
-    code: number,
-    message: string
+  name: string;
+  code: number;
+  message: string;
 }
 /**
  * Erros base de jogo. Esses erros devem ser enviados para o usuário e não agir como um erro mesmo
  */
 export class GameError {
-    public error: Err;
-    public gameError: IGameException;
+  public error: Err;
+  public gameError: IGameException;
 
-    constructor(gameError: IGameException) {
-        this.gameError = gameError;
-        this.error = new Err(SocketEvents.GameError, this.gameError);
-    }
+  constructor(gameError: IGameException) {
+    this.gameError = gameError;
+    this.error = new Err(SocketEvents.GameError, this.gameError);
+  }
 
-    // Envia o erro ao cliente
-    sendToClient(client: APISocket) {
-        client.emit(SocketEvents.GameError, this.gameError);
-    }
+  // Envia o erro ao cliente
+  sendToClient(client: APISocket) {
+    client.emit(SocketEvents.GameError, this.gameError);
+  }
 
-    // Ativa o erro
-    raise() {
-        throw this.error;
-    }
+  // Ativa o erro
+  raise() {
+    throw this.error;
+  }
 }
